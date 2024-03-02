b122fce8684722ef4facd116c4657c07849aea69
AkshatGadhwal <akshatgadhwal1@gmail.com>
2024-03-02 07:04:11 +0530

    adding consumer and producer to git

---
+++ producer.py
@@ -0,0 +1,22 @@
+from faker import Faker
+import json
+import random
+import time
+from kafka import KafkaProducer
+
+faker = Faker()
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+user_ids = [faker.uuid4() for _ in range(100)]  # Generate a list of 100 unique user IDs
+
+while True:
+    event = {
+        "timestamp": faker.iso8601(),
+        "userId": random.choice(user_ids),  # Randomly select a user ID from the list
+        "eventType": "pageView",
+        "productId": faker.uuid4(),
+        "sessionDuration": faker.random_int(min=0, max=300)
+    }
+    producer.send('user-events', json.dumps(event).encode('utf-8'))
+    time.sleep(0.1)
+
